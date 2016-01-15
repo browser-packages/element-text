9d7a8d7f9f71465045fd95b6a5034fc2a71c14bc
Gabriel Conceição <gabriel.conceicao@findhit.com>
2016-01-15 17:02:32 +0000

    Open-Source

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ module.exports = function ( text ) {
     var prop =
         'textContent' in this && 'textContent' ||
         'innerText' in this && 'innerText' ||
-        'innerHTML';
+        'innerHTML'
 
-    // Set innerHTML
+    // Set
     if( arguments.length === 1 && typeof text !== 'undefined' ) {
-        this[ prop ] = text;
-        return this;
+        this[ prop ] = text
+        return this
     }
 
-    return this[ prop ] || '';
-};
+    // Get
+    return this[ prop ] || ''
+}
